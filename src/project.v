ac31970feae48ea959b36fd093b9c5d3b67158f4
parkwilliamy <parkwilliamy@gmail.com>
2025-10-24 13:11:09 -0400

    wrote initial design for spi module

--- src/project.v
+++ src/project.v
@@ -21,13 +21,59 @@ module tt_um_uwasic_onboarding_WillPark (
   wire [7:0] en_reg_pwm_7_0;
   wire [7:0] en_reg_pwm_15_8;
   wire [7:0] pwm_duty_cycle;
+
+  assign en_reg_out_7_0 = rst_n ? addr0 : 0;
+  assign en_reg_out_15_8 = rst_n ? addr1 : 0;
+  assign en_reg_pwm_7_0 = rst_n ? addr2 : 0;
+  assign en_reg_pwm_15_8 = rst_n ? addr3 : 0;
+  assign pwm_duty_cycle = rst_n ? addr4 : 0;
+
+  reg [7:0] addr0, addr1, addr2, addr3, addr4;
   
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe = 8'hFF; // Set all IOs to output
 
+  // Instantiate the PWM module
+  pwm_peripheral pwm_peripheral_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .en_reg_out_7_0(en_reg_out_7_0),
+    .en_reg_out_15_8(en_reg_out_15_8),
+    .en_reg_pwm_7_0(en_reg_pwm_7_0),
+    .en_reg_pwm_15_8(en_reg_pwm_15_8),
+    .pwm_duty_cycle(pwm_duty_cycle),
+    .out({uio_out, uo_out})
+  );
+
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, ui_in[7:3], uio_in, 1'b0};
+  
+  wire SCLK, COPI, nCS;
+  assign SCLK = ui_in[0];
+  assign COPI = ui_in[1];
+  assign nCS = ui_in[2];
+
+  // CDC Registers
+  reg COPI_1;
+
+  spi spi_inst (
+    .rst_n(rst_n),
+    .SCLK(SCLK),
+    .COPI(COPI_1),
+    .nCS(nCS),
+    .addr0(addr0),
+    .addr1(addr1),
+    .addr2(addr2),
+    .addr3(addr3),
+    .addr4(addr4)
+  );
+
+  always @ (posedge clk) begin
+
+    COPI_1 <= COPI;
+
+  end
 
 endmodule
---
+++ src/spi.v
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2024 Will Park
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module spi (
+    input wire rst_n, SCLK, COPI, nCS,
+    output reg [7:0] addr0, addr1, addr2, addr3, addr4
+);
+
+  reg [1:0] current_state, next_state;
+  localparam IDLE = 0, WRITE = 1, ADDRESS = 2, DATA = 3;
+
+  reg [6:0] addr;
+  reg [2:0] addr_index;
+  localparam MAX_ADDR = 4;
+
+  reg [7:0] data;
+  reg [2:0] data_index;
+
+  // CDC Registers
+  reg COPI_2;
+
+  always @ (negedge rst_n) begin
+
+    current_state <= IDLE;
+    next_state <= IDLE;
+    addr_index <= 0;
+    data_index <= 0;
+
+  end
+
+  always @ (posedge SCLK) begin
+
+    COPI_2 <= COPI;
+    current_state <= next_state;
+
+  end
+
+  always @ (negedge SCLK) begin
+
+    if (current_state == ADDRESS) begin
+
+        addr_index <= addr_index + 1;
+
+    end
+
+    if (current_state == DATA) begin
+
+        data_index <= data_index + 1; //shift data out on -edge
+
+    end
+
+  end
+
+  always @ (*) begin
+
+    case (current_state) 
+
+      IDLE: begin
+
+        if (!nCS) next_state = WRITE;
+
+      end
+
+      WRITE: begin
+
+        if (COPI_2) begin
+          next_state = ADDRESS;
+          addr_index = 0;
+          data_index = 0;
+        end
+        else next_state = IDLE; //ignore reads 
+
+      end
+
+      ADDRESS: begin
+
+        addr[addr_index] = COPI_2;
+        if (addr_index <= 6) next_state = ADDRESS;    
+        else begin
+          if (addr <= MAX_ADDR) next_state = DATA; 
+          else next_state = IDLE; //reject invalid addresses
+        end
+
+      end
+
+      DATA: begin
+
+        data[data_index] = COPI_2;
+        if (data_index <= 7) next_state = DATA;
+        else begin
+
+          if (nCS) begin
+
+            case (addr)
+
+            0: addr0 = data;
+            1: addr1 = data;
+            2: addr2 = data;
+            3: addr3 = data;
+            4: addr4 = data;
+
+            endcase
+
+            next_state = IDLE;
+
+          end
+
+        end
+
+      end
+
+  
+
+    endcase
+
+  end
+
+endmodule
